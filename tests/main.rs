eb519b3e8334ec7f95d1dd17b5a04e0928d4eb84
Joshua Smart <josh@thesmarts.co.uk>
2023-01-04 23:31:36 +0000

    Added junit report to test framework

--- tests/main.rs
+++ tests/main.rs
@@ -1,9 +1,20 @@
-use cucumber::WorldInit;
+use std::fs;
+
+use cucumber::{writer, WorldInit, WriterExt};
 
 mod common;
 use common::state::State;
 
 #[tokio::main]
 async fn main() {
-    State::run("tests/features").await;
+    let file = fs::File::create("junit.xml").unwrap();
+    let writer = writer::Basic::stdout()
+        .summarized()
+        .tee::<State, _>(writer::JUnit::for_tee(file, 0))
+        .normalized();
+
+    State::cucumber()
+        .with_writer(writer)
+        .run("tests/features")
+        .await;
 }
